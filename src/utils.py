678b095b4c950b2bfd7b12204fc046b4d7a6ee15
evgenij.scherbakov <evggenshch@gmail.com>
2020-07-30 02:58:51 +0300

    collate function changed to custom

--- src/utils.py
+++ src/utils.py
@@ -83,32 +83,47 @@ class collateFunction():
 
         for imageset in batch:
 
+
             lrs = imageset['lr']
             L, H, W = lrs.shape
-
-            if L >= self.min_L:  # pad input to top_k
-                lr_batch.append(lrs[:self.min_L])
-                alpha_batch.append(torch.ones(self.min_L))
-            else:
-                pad = torch.zeros(self.min_L - L, H, W)
-                lr_batch.append(torch.cat([lrs, pad], dim=0))
-                alpha_batch.append(torch.cat([torch.ones(L), torch.zeros(self.min_L - L)], dim=0))
-
+            lr_batch.append(lrs)
+            alpha_batch.append(torch.ones(L))
             hr = imageset['hr']
-            if train_batch and hr is not None:
-                hr_batch.append(hr)
-            else:
-                train_batch = False
-
+            hr_batch.append(hr)
             hm_batch.append(imageset['hr_map'])
             isn_batch.append(imageset['name'])
 
-        padded_lr_batch = torch.stack(lr_batch, dim=0)
-        alpha_batch = torch.stack(alpha_batch, dim=0)
 
-        if train_batch:
-            hr_batch = torch.stack(hr_batch, dim=0)
-            hm_batch = torch.stack(hm_batch, dim=0)
+        padded_lr_batch = lr_batch
+
+ #       for imageset in batch:#
+
+ #           lrs = imageset['lr']
+ #           L, H, W = lrs.shape
+
+ #           if L >= self.min_L:  # pad input to top_k
+ #               lr_batch.append(lrs[:self.min_L])
+ #               alpha_batch.append(torch.ones(self.min_L))
+ #           else:
+ #               pad = torch.zeros(self.min_L - L, H, W)
+ #               lr_batch.append(torch.cat([lrs, pad], dim=0))
+ #               alpha_batch.append(torch.cat([torch.ones(L), torch.zeros(self.min_L - L)], dim=0))
+
+ #           hr = imageset['hr']
+ #           if train_batch and hr is not None:
+ #               hr_batch.append(hr)
+ #           else:
+ #               train_batch = False
+
+ #           hm_batch.append(imageset['hr_map'])
+ #           isn_batch.append(imageset['name'])
+
+ #       padded_lr_batch = torch.stack(lr_batch, dim=0)
+ #       alpha_batch = torch.stack(alpha_batch, dim=0)
+
+  #      if train_batch:
+  #          hr_batch = torch.stack(hr_batch, dim=0)
+  #          hm_batch = torch.stack(hm_batch, dim=0)
 
         return padded_lr_batch, alpha_batch, hr_batch, hm_batch, isn_batch
 
